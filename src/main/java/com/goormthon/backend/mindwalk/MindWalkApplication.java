febec22886b43018e745a1a663d859590e4189ee
so1eeee <solball@naver.com>
2025-08-29 14:14:23 +0900

    init

---
+++ src/main/java/com/goormthon/backend/mindwalk/MindWalkApplication.java
@@ -0,0 +1,13 @@
+package com.goormthon.backend.mindwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindWalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindWalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormthon/backend/mindwalk/MindWalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.backend.mindwalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindWalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
