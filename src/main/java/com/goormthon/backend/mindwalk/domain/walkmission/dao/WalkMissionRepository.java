0f9cb4ba9ded3e76c8cd9f64e9ea391513a51be0
so1eeee <solball@naver.com>
2025-09-05 17:01:10 +0900

    [feat/#25] 주간 요약 조회 API 구현

--- src/main/java/com/goormthon/backend/mindwalk/domain/walkmission/dao/WalkMissionRepository.java
+++ src/main/java/com/goormthon/backend/mindwalk/domain/walkmission/dao/WalkMissionRepository.java
@@ -1,10 +1,20 @@
 package com.goormthon.backend.mindwalk.domain.walkmission.dao;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.goormthon.backend.mindwalk.domain.user.domain.User;
 import com.goormthon.backend.mindwalk.domain.walkmission.domain.WalkMission;
+import com.goormthon.backend.mindwalk.domain.walkmission.domain.WalkMissionStatus;
 
 @Repository
 public interface WalkMissionRepository extends JpaRepository<WalkMission, Long> {
+	List<WalkMission> findAllByUserAndStatusAndCompletedAtBetween(User user, WalkMissionStatus status,
+		LocalDateTime start, LocalDateTime end);
+
+	boolean existsByUserAndStatusAndCompletedAtBetween(User user, WalkMissionStatus status, LocalDateTime start,
+		LocalDateTime end);
 }
