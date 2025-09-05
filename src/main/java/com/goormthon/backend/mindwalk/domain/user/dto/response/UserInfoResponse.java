01ae90be94f89c5f903a657110808808bde98077
so1eeee <solball@naver.com>
2025-09-06 02:55:33 +0900

    [feat/#31] 마이페이지에 산책 횟수 및 피운 꽃 개수 필드 추가

--- src/main/java/com/goormthon/backend/mindwalk/domain/user/dto/response/UserInfoResponse.java
+++ src/main/java/com/goormthon/backend/mindwalk/domain/user/dto/response/UserInfoResponse.java
@@ -2,12 +2,24 @@ package com.goormthon.backend.mindwalk.domain.user.dto.response;
 
 import com.goormthon.backend.mindwalk.domain.user.domain.User;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserInfoResponse(
+	@Schema(description = "사용자 ID", example = "1")
 	Long userId,
-	String nickname
-	// boolean allowPushNotification
+	@Schema(description = "닉네임", example = "닉네임")
+	String nickname,
+	@Schema(description = "마음 산책한 날", example = "35")
+	Long totalWalkCount,
+	@Schema(description = "피운 꽃 개수", example = "100")
+	Long bloomedFlowerCount
 ) {
-	public static UserInfoResponse from(User user) {
-		return new UserInfoResponse(user.getId(), user.getNickname());
+	public static UserInfoResponse of(User user, Long totalWalkCount, Long bloomedFlowerCount) {
+		return new UserInfoResponse(
+			user.getId(),
+			user.getNickname(),
+			totalWalkCount,
+			bloomedFlowerCount
+		);
 	}
 }
